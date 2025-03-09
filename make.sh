61385c6218cd57fdd4ad3b8abb8397ec948ae3e2
Luis Peñarrubia <luis.penarrubia@gmail.com>
2025-03-09 17:47:15 +0100

    fix permissions

--- make.sh
+++ make.sh
@@ -3,7 +3,6 @@
 echo "Building API documentation..."
 
 # Build the API documentation
-
 npx @redocly/cli bundle -o sfgoa3.yaml && \
 npx @redocly/cli build-docs sfgoa3.yaml -o index.html
 # Build the API documentation
